98ab64064d587c13cc21e16b5a33b4f1aeb1cdbb
Marcos Leandro <marcosleandrocxs-cloud@example.com>
2026-06-29 17:43:51 -0300

    fix: ATUALIZACAO VISUAL

--- build/web/main.dart.js_15.part.js
+++
@@ -1,6 +0,0 @@
-((a,b)=>{a[b]=a[b]||{}})(self,"$__dart_deferred_initializers__")
-$__dart_deferred_initializers__.current=function(a,b,c,$){var A,B
-A=c[0]
-B=c[18]
-var z=a.updateTypes([]);(function constants(){B.HQ=new A.d5(8,null,null,null)})()};
-(a=>{a["97bzF/rTyyrI2WBKBxpX3vEAe7I="]=a.current})($__dart_deferred_initializers__);
--- build/web/main.dart.js_4.part.js
+++
@@ -1,6 +0,0 @@
-((a,b)=>{a[b]=a[b]||{}})(self,"$__dart_deferred_initializers__")
-$__dart_deferred_initializers__.current=function(a,b,c,$){var A,B
-A=c[0]
-B=c[14]
-var z=a.updateTypes([]);(function constants(){B.Ic=new A.d1(10,null,null,null)})()};
-(a=>{a["l0CN+1xwa/VowkDz4sZBCHPN68s="]=a.current})($__dart_deferred_initializers__);
